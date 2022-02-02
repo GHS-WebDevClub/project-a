20c8d4f00de3f6efdebf89db35537797502421e0
Aubin C. Spitzer <aubin@uthd.dev>
2022-02-02 13:19:40 -0800

    Working commit

--- pages/api/v1/examples/index.ts
+++ pages/api/v1/examples/index.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == "GET") {
-    res.status(200).end("Base route for API examples");
+    res.status(200).json({ users: ["aubin", "theo", "ben"] });
   }
 };
