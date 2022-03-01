18cc11c655982dcf29eb40e79b714a783e8755f5
uthd-dev <59554139+aubincspitzer@users.noreply.github.com>
2022-02-28 22:59:47 -0800

    Functional /api/v1/courses/ endpoint
    Class -> Course to avoid JS naming conflicts
    
    Teacher & Course type -> class, for constructors & re-usability
    
    Added type from user profiles to standardize, it should already match NextAuth for the time being.

---
+++ pages/api/v1/courses/index.ts
@@ -0,0 +1,82 @@
+/**
+ * Handles Creation and (while in development, ) listing of all courses
+ *
+ * Created by Aubin C. Spitzer (@aubincspitzer) on 02/28/2022
+ */
+
+import { Db } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { ResponseData } from "../../../../types/api/ResponseData.type";
+import { Course } from "../../../../types/db/course.type";
+import apiLogger, { ApiMsg } from "../../../../utils/api/Logger";
+import clientPromise from "../../../../utils/db/connect";
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  //Check session
+  if (!(await getSession({ req })))
+    return res
+      .status(401)
+      .json({ error: "You must be signed in to access this content." });
+
+  //Handle logged-in state
+  if (req.method == "GET" || req.method == "POST") {
+    const db = (await clientPromise).db();
+
+    switch (req.method) {
+      case "GET":
+        const courses = await getCourses(db);
+        if (!courses)
+          return res.status(500).json({ error: "Failed to retrieve courses!" });
+
+        return res.status(200).json({ result: courses });
+      case "POST":
+        console.log("yo");
+        const data = JSON.parse(req.body);
+        const course = await createCourse(db, data);
+        if (!course)
+          return res.status(500).json({ error: "Failed to create course!" });
+      //TODO: Add support for course details to be updated / figure out how this will work.
+    }
+  } else res.status(404).json({ error: 404 });
+};
+
+//Retrieve all courses from database
+async function getCourses(db: Db): Promise<undefined | Array<any>> {
+  try {
+    const courses = await db.collection("courses").find().toArray();
+    return courses;
+  } catch (err) {
+    console.log(err);
+    apiLogger(
+      new ApiMsg(
+        "Failed to retrieve all courses from database!",
+        "MAJ",
+        "GET /api/v1/courses/"
+      )
+    );
+    return;
+  }
+}
+
+//Create req.body as new course in database
+async function createCourse(db: Db, data: any) {
+  try {
+    const course = new Course(data.displayName);
+    await db.collection("courses").insertOne(course);
+    return course;
+  } catch (err) {
+    console.log(err);
+    apiLogger(
+      new ApiMsg(
+        `Failed to create new course ${data._id?.toString()}`,
+        "MAJ",
+        "POST /api/v1/courses/"
+      )
+    );
+    return;
+  }
+}
--- types/db/teacher.type.ts
+++ types/db/teacher.type.ts
@@ -1,20 +1,34 @@
 /**
  * Created by Aubin C. Spitzer (@aubincspitzer) on 02/28/2022
- * 
+ *
  * Describes a teacher's profile (to be associated with classes)
  */
 
-export type Teacher = {
-    id: string;
-    profile: {
-        email: string;
-        displayName: string;
-        image: string;
-    }
-    verification: {
-        verifiedAt?: string;
-        verifiedBy?: string;
-    }
-    createdAt: string;
-    lastModifiedAt: string;
+import { DateTime } from "luxon";
+import { ObjectId } from "mongodb";
+import { ProfileType } from "./profile.type";
+
+export type verifiedCoursesType = Array<{
+  courseId: ObjectId;
+  verifiedAt: string;
+  verifiedBy: ObjectId;
+}>;
+
+export class Teacher {
+  _id: ObjectId;
+  profile: ProfileType;
+  verifCourses: Array<verifiedCoursesType>;
+  createdAt: string;
+  lastModifiedAt: string;
+
+  constructor(
+    profile: ProfileType,
+    initialVerifCourses?: Array<verifiedCoursesType>
+  ) {
+    this._id = new ObjectId();
+    this.profile = profile;
+    this.createdAt = DateTime.now().toISO();
+    this.lastModifiedAt = this.createdAt;
+    this.verifCourses = initialVerifCourses || [];
+  }
 }
