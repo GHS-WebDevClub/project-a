c5b3bf637f66dfe11bba79fdb80e10f080ca3a5b
BuildTools <jsbedrock206@gmail.com>
2022-03-16 17:04:40 -0700

    Update index.ts

--- pages/api/v1/users/[username]/index.ts
+++ pages/api/v1/users/[username]/index.ts
@@ -1,37 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { ResponseData } from '../../../../../types/api/ResponseData.type';
-import type { UserObject } from '../../../../../types/api/UserObject.type';
+import type { ResponseData } from "../../../../../types/api/ResponseData.type";
+import type { UserObject } from "../../../../../types/api/UserObject.type";
 import { getSession } from "next-auth/react";
 import { Db } from "mongodb";
 import clientPromise from "../../../../../utils/db/connect";
 
-
 export default async (
-    req: NextApiRequest, 
-    res: NextApiResponse<ResponseData>
-    ) =>{
-        //Validate session
-        if (!(await getSession({ req })))
-            return res.status(401).json({ error: "You must be signed in to access this content." });
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  //Validate session
+  if (!(await getSession({ req })))
+    return res
+      .status(401)
+      .json({ error: "You must be signed in to access this content." });
 
-        //If logged in, check if method is valid for this API path:
-        if(req.method === 'GET' || req.method === 'POST') {
-            const db = (await clientPromise)
+  //If logged in, check if method is valid for this API path:
+  if (req.method === "GET" || req.method === "POST") {
+    //const db = (await clientPromise)
 
-            switch (req.method) {
-                case "GET":
-                    /*if username exists in the users collection
+    switch (req.method) {
+      case "GET":
+        const { username } = req.query;
+        /*if username exists in the users collection
                         return contents of object with the matching name field 
                     else return error*/
-                case "POST":
-                    /*
-                    This API method would be called by the application after a user has filled out a name, email, and password.
-                    If the name or email exist already, this should be terminated.
+        if (!(typeof username === "string"))
+          return res.status(400).json({ error: "Malformed request!" });
 
-                    if object exists in the users collection with name || email
-                        throw error and terminate process (Already in use)
-                    else create new object with params
-                    */
-            }
-        }else return res.status(404)
+        const collection = (await clientPromise).db().collection("members");
+        const member = await collection.findOne(
+          { username: username },
+          { projection: { username: 1, _id: 0 } }
+        );
+        if (!member)
+          return res.status(500).json({ error: "Internal server error" });
+        return res.status(200).json({ result: member });
+      case "POST":
+        /*
+        This API method would be called by the application after a user has filled out a displayname, email, and username.
+        If the name or email exist already, this should be terminated.
+
+        if object exists in the users collection with name || email
+            throw error and terminate process (Already in use)
+        else create new object with params
+        */
+        break;
+    }
+  } else return res.status(404);
 };
