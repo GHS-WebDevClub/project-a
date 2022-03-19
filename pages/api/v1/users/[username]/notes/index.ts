c052bf1890b9d0f142b540998e8145e0e12fe050
Aubin C. Spitzer <aubin@uthd.dev>
2022-03-19 09:49:41 -0700

    WIP /users/[username]/notes

--- pages/api/v1/users/[username]/notes/index.ts
+++ pages/api/v1/users/[username]/notes/index.ts
@@ -8,9 +8,11 @@
  * POST /users/<username>/notes - Submits a new note with data to server (TODO aubin)
  */
 
+import { Db, ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { ResponseData } from "../../../../../../types/api/ResponseData.type";
+import clientPromise from "../../../../../../utils/db/connect";
 
 export default async function (
   req: NextApiRequest,
@@ -22,12 +24,38 @@ export default async function (
       .status(401)
       .json({ error: "You must be signed in to access this content." });
 
+  if (!(req.method == "GET" || req.method == "POST"))
+    return res.status(405).json({ error: "Method not allowed" });
+
+  const db = (await clientPromise).db();
+  const { username } = req.query;
+
+  if (typeof username !== "string")
+    return res.status(400).json({ error: "Missing username!" });
+
   switch (req.method) {
     case "GET":
-      res.status(500).json({ error: "Not implemented yet!" });
+      const { assigned } = req.query;
+      const isAssigned = (typeof assigned == "string" && assigned == "true");
+
+      const notes = await getNotes(db, username, isAssigned);
+
+      
+
       break;
     case "POST":
       res.status(500).json({ error: "Not implemented yet!" });
       break;
+  }
+}
+
+async function getNotes(db: Db, username: string, assigned: boolean) {
+  try {
+    const notes = db.collection("notes").find({
+      meta: { authorId: new ObjectId(username) },
+      details: assigned ? { dueAt: { $exists: true } } : {},
+    });
+  }catch(err) {
+
   }
 }
