881a6fbe0addd298b20c7e7816503e5b9d0614ee
C34A <theo@seelye.net>
2022-03-30 13:22:30 -0700

    i don't know what i'm doing

--- pages/api/v1/users/[username]/settings/index.ts
+++ pages/api/v1/users/[username]/settings/index.ts
@@ -3,27 +3,40 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { useRouter } from 'next/router';
 import { getSession } from "next-auth/react";
 import { UserSettings } from '../../../../../../types/api/UserObject.type';
+import clientPromise from '../../../../../../utils/db/connect';
+import { ResponseDataT } from '../../../../../../types/api/ResponseData.type';
+import Member from '../../../../../../types/db/member.type';
+import { ProfileType } from '../../../../../../types/db/profile.type';
 
-type Response = UserSettings  // GET success response
-              | {err: string} // failure
-              | "OK";         // POST success
-
-export default (req: NextApiRequest, res: NextApiResponse<Response>) => {
+export default (req: NextApiRequest, res: NextApiResponse<ResponseDataT<ProfileType>>) => {
   switch (req.method) {
     case "GET": getSettings(req, res); break;
     case "POST": postSettings(req, res); break;
     default: {
-      res.status(500).send({err: "internal server error"});
+      res.status(404).send({error: 404});
     }
   }
 }
 
-function getSettings(req: NextApiRequest, res: NextApiResponse<Response>) {
-  res.status(500).send({err: "Not implemented!"});
+async function getSettings(req: NextApiRequest, res: NextApiResponse<ResponseDataT<ProfileType>>) {
+  try {
+    const db = (await clientPromise).db();
+    const {username} = req.query;
+    const user = await db.collection("members").findOne(
+        { username: username }
+      );
+    const prof = (user as Member).profile;
+
+    res.status(200).send({result: prof});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({error: "internal server error!"});
+  }
+
 }
 
-function postSettings(req: NextApiRequest, res: NextApiResponse<Response>) {
-  const newSettings: UserSettings = req.body;
+function postSettings(req: NextApiRequest, res: NextApiResponse<ResponseDataT<ProfileType>>) {
+  const newSettings: ProfileType = req.body;
   // todo: send to database?
-  res.status(500).send({err: "Not implemented!"});
+  res.status(500).send({error: "Not implemented!"});
 }
--- types/api/ResponseData.type.ts
+++ types/api/ResponseData.type.ts
@@ -1,4 +1,12 @@
+
+/**
+ * @deprecated
+ */
 export type ResponseData = {
     error?: string | number;
     result?: Object | Array<any> | string;
-}
+};
+
+export type ResponseDataT<T> = 
+    {error?: string | number} |
+    {result?: T};
