4b660c33c41f826a315167bd2ea58c9effd36113
aubincspitzer <59554139+aubincspitzer@users.noreply.github.com>
2022-04-14 23:25:25 -0700

    Major WIP
    We might need to revert to ResponseData over ResponseDataT; More testing / research required

--- components/LoginFlow/RegisterForm.tsx
+++ components/LoginFlow/RegisterForm.tsx
@@ -1,23 +1,33 @@
 /**
  * Created by Aubin C. Spitzer (@aubincspitzer) on 04/14/2022
- * 
+ *
  * Component for collecting first-time user info after initial O-auth flow succeeds
  * Collects info, then POSTs to member registration API route
  */
 
-import Container from "../PageContainer";
-import { TextInput, IconButton } from "@ghs-wdc/storybook"
+import { TextInput, IconButton } from "@ghs-wdc/storybook";
 import styled from "styled-components";
 import React, { FormEvent, useEffect, useState } from "react";
+import { ResponseData } from "../../types/api/ResponseData.type";
 
-type StateType = IIndexable & { uname?: string, fname?: string, lname?: string, phone?: string };
+type StateType = IIndexable & {
+    uname?: string;
+    fname?: string;
+    lname?: string;
+    phone?: string;
+};
 
 interface IIndexable {
     [key: string]: string;
 }
 
 export default function RegisterForm() {
-    const [formState, setFormState] = useState<StateType>({ "uname": "", "fname": "", "lname": "", "phone": "" });
+    const [formState, setFormState] = useState<StateType>({
+        uname: "",
+        fname: "",
+        lname: "",
+        phone: "",
+    });
 
     function handleInputChange(e: FormEvent<HTMLInputElement>) {
         const target = e.currentTarget;
@@ -40,7 +50,35 @@ export default function RegisterForm() {
         setFormState(newState);
     }
 
-    function handleSubmit() {
+    async function handleSubmit(animationCallback: () => void): Promise<boolean> {
+        const newMember = {
+            displayName: `${formState.fname} ${formState.lname}`,
+            uname: formState.uname,
+            phone: formState.phone,
+        };
+
+        try {
+
+            const res = await fetch("/api/v1/users", {
+                body: JSON.stringify(newMember), headers: {
+                    "Content-Type": "application/json"
+                }, method: "POST"
+            })
+
+
+            if (res.status !== 200) return false;
+
+            const data: ResponseData = await res.json();
+
+            if(data.result) return true;
+
+            return false;
+
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+
         /**
          * TODO:
          * - Submittal of Data to api route
@@ -50,34 +88,69 @@ export default function RegisterForm() {
     }
 
     return (
-        <Form action="/api/v1/users/" method="POST" id="register" onSubmit={(e) => e.preventDefault()}>
+        <Form
+            action="/api/v1/users/"
+            method="POST"
+            id="re<gister"
+            onSubmit={(e) => e.preventDefault()}
+        >
             <NameContainer>
                 {/**
-                 * Using onBlur instead of onChange since React emits onInput events as onChange events.
-                 * (This way I can still have vanilla onChange-like functionality)
-                 */}
-                <TextInput id="fname" name="fname" onBlur={handleInputChange} placeholder="First" />
-                <TextInput id="lname" name="lname" onBlur={handleInputChange} placeholder="Last" />
+         * Using onBlur instead of onChange since React emits onInput events as onChange events.
+         * (This way I can still have vanilla onChange-like functionality)
+         */}
+                <TextInput
+                    id="fname"
+                    name="fname"
+                    onBlur={handleInputChange}
+                    placeholder="First Name"
+                />
+                <TextInput
+                    id="lname"
+                    name="lname"
+                    onBlur={handleInputChange}
+                    placeholder="Last Name"
+                />
             </NameContainer>
-            <TextInput id="uname" name="uname" onBlur={handleInputChange} placeholder={formState.uname ? formState.uname : "Username"} defaultValue={formState.uname} />
-            <TextInput id="phone" name="phone" onBlur={handleInputChange} placeholder="Phone" />
-            <IconButton primary disabled={(formState.fname && formState.lname && formState.uname) ? false : true} />
+            <TextInput
+                id="uname"
+                name="uname"
+                onBlur={handleInputChange}
+                placeholder={formState.uname ? formState.uname : "Username"}
+                defaultValue={formState.uname}
+            />
+            <TextInput
+                id="phone"
+                name="phone"
+                type="tel"
+                pattern="^\+?\d{0,13}"
+                minLength={10}
+                onBlur={handleInputChange}
+                placeholder="Phone"
+            />
+            <IconButton
+                primary
+                disabled={
+                    formState.fname && formState.lname && formState.uname ? false : true
+                }
+                action={handleSubmit}
+            />
         </Form>
     );
 }
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  width: fit-content;
+  height: 20rem;
 `;
 
 const NameContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
-    width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  width: 100%;
 `;
