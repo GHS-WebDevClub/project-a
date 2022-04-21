36b848eb9b00f2ecc1268beb622713762692e0ce
uthd-dev <59554139+aubincspitzer@users.noreply.github.com>
2022-04-20 22:55:37 -0700

    Recreated GET /users/<username>
    Also made ajustments to API response class: ResponseUni and it's corresponding Error class

--- pages/api/v1/users/[username]/index.ts
+++ pages/api/v1/users/[username]/index.ts
@@ -1,74 +1,120 @@
 /**
- * Handles GET and POST API requests to /api/v1/users/[username]
+ * Handles GET API requests to /api/v1/users/[username]
  *
  * Created by Jack Sanford (@bedrock206) on 03/07/2022
+ *
+ * Rewritten by Aubin Spitzer (@aubincspitzer) on 04/20/2022 (lul 420)
+ * Based on:
+ * GET /users/<username> - returns specifc user object with username, displayname, UUID, email | This route should be session-protected.
  */
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { ResponseDataT } from "../../../../../types/api/ResponseData.type";
+import {
+  ResponseUni,
+} from "../../../../../types/api/ResponseData.type";
 //import type { UserObject } from "../../../../../types/api/UserObject.type";
 import { getSession } from "next-auth/react";
-import { Db } from "mongodb";
 import clientPromise from "../../../../../utils/db/connect";
 import Member from "../../../../../types/db/member.type";
+import { ApiError } from "../../../../../types/api/ResponseData.type";
+import {
+  PrivateProfile,
+  PublicProfile,
+} from "../../../../../types/db/profile.type";
+import checkSessionUsername from "../../../../../utils/api/v1/checkSessionUsername";
+
+export type DynMemberProfileType =
+  | (PublicProfile & (PrivateProfile | null))
+  | null;
 
 export default async (
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseDataT<string | Member | {}>>
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseUni<DynMemberProfileType>>
 ) => {
-    //Validate session
-    if (!(await getSession({ req })))
-        return res
-            .status(401)
-            .json({ error: "You must be signed in to access this content." });
+  //Check session
+  if (!(await getSession({ req })))
+    return res
+      .status(401)
+      .json(
+        new ResponseUni(
+          [
+            new ApiError(
+              "auth-001",
+              "You must be signed in to access this content.",
+              "401 Unauthorized - Missing Session"
+            ),
+          ],
+          req.url
+        )
+      );
 
-    //If logged in, check if method is valid for this API path:
-    if (req.method === "GET" || req.method === "POST") {
-        switch (req.method) {
-            case "GET": {
-                const { username } = req.query;
+  //Check Method
+  if (!(req.method == "GET"))
+    return res
+      .status(404)
+      .json(
+        new ResponseUni(
+          [
+            new ApiError(
+              "req-001",
+              `400 Bad Request`,
+              `400 Bad Request - Invalid Method "${req.method}"`
+            ),
+          ],
+          req.url
+        )
+      );
 
-                if (!(typeof username === "string"))
-                    //If the request is formatted incorrectly, throw error
-                    return res
-                        .status(400)
-                        .json({ error: "Malformed request!" });
+  const { username } = req.query;
 
-                const collection = (await clientPromise)
-                    .db()
-                    .collection("members");
-                const member = await collection.findOne(
-                    { username: username },
-                    { projection: { _id: 0, profile: 1 } }
-                );
-                return res.status(200).json({ result: member ? member as Member : {} });
-            }
-            case "POST":
-                {
-                    const { username } = req.query;
+  //Check username from URL
+  if (!(typeof username === "string"))
+    return res
+      .status(400)
+      .json(
+        new ResponseUni(
+          [
+            new ApiError(
+              "req-002",
+              "Invalid API request",
+              "400 Bad Request - Invalid or missing URL parameter"
+            ),
+          ],
+          req.url
+        )
+      );
 
-                    if (!(typeof username === "string")) //If the request is formatted incorrectly, throw error
-                        return res
-                            .status(400)
-                            .json({ error: "Malformed request!" });
+  //Store if username matches session
+  const inclPrivProf = await checkSessionUsername(req, username);
 
-                    const collection = (await clientPromise)
-                        .db()
-                        .collection("members");
-                    const member = await collection.findOne(
-                        { username: username },
-                        { projection: { _id: 0, profile: 1 } })
-                    if (!(member === null))  return res.status(500).json({ error: "User already exists!"}) //If you get a result back, the user already exists
-                    //return res.status(200).json({result: `${req.query.username} is not a user that exists yet`})
-                    //if (!(req.body ===)) {}
-                }
-        }
-    } else return res.status(404);
+  //Get member from DB by username
+  const collection = (await clientPromise).db().collection("members");
+  const member = (await collection.findOne(
+    { username: username },
+    { projection: { _id: 0, profile: 1 } }
+  )) as Member | null;
+
+  //Check member exists
+  if (!member)
+    return res
+      .status(200)
+      .json(
+        new ResponseUni(
+          [
+            new ApiError(
+              "dat-001",
+              `The user ${username} could not be found!`,
+              `200 OK - Item "${username}" does not exist in the Database`
+            ),
+          ],
+          req.url,
+          null
+        )
+      );
+
+  //Generate dynamic profile data based on permissions
+  const privProf = inclPrivProf ? member.profile.private : {};
+  const dynMemberProf = { ...member.profile.public, ...privProf };
+
+  return res.status(200).json(new ResponseUni([], req.url, dynMemberProf));
 };
-
-type ValidUserCreationObject = {
-    firstname: string,
-    lastname: string,
-    email: string,
-
-}
